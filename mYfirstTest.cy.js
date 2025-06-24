2fb4414e31cb62bcd6323484c5dad8df7461f33e
Sneha456244 <151616854+Sneha456244@users.noreply.github.com>
2025-06-24 11:47:08 +0530

    Add files via upload

---
+++ mYfirstTest.cy.js
@@ -0,0 +1,7 @@
+describe('My First Test', () => {
+  it('test1', () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    cy.title().should('eq','OrangeHRM')
+  })
+})
